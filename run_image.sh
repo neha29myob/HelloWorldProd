d3d060373de80d5c8c4f7c35809fcf87baad02ba
Kalyani Singh <kalyani.singh@myob.com>
2018-05-04 10:25:07 +1000

    fixed run_image script

--- run_image.sh
+++ run_image.sh
@@ -1,1 +1,1 @@
-docker run -p -8000:8000 451515956712.dkr.ecr.ap-southeast-2.amazonaws.com/neha-hello-world
+docker run -p 8000:8000 451515956712.dkr.ecr.ap-southeast-2.amazonaws.com/neha-hello-world
