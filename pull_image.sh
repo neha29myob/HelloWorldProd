90d5cb8bf1bf15ab2140895f6ea34bb307161369
Kalyani Singh <kalyani.singh@myob.com>
2018-05-06 22:30:39 +1000

    changed back to pull image

---
+++ pull_image.sh
@@ -0,0 +1,2 @@
+$(aws ecr get-login --no-include-email --region ap-southeast-2)
+docker pull 451515956712.dkr.ecr.ap-southeast-2.amazonaws.com/neha-hello-world:latest
--- pull_run_image.sh
+++
@@ -1,4 +0,0 @@
-$(aws ecr get-login --no-include-email --region ap-southeast-2)
-docker pull 451515956712.dkr.ecr.ap-southeast-2.amazonaws.com/neha-hello-world:latest &&
-docker run -p 8000:8000 451515956712.dkr.ecr.ap-southeast-2.amazonaws.com/neha-hello-world
-
